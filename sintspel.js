b78d7e7b3b73a486b60b81092e6c35870e90c361
Max <65962153+Blue-Sea2@users.noreply.github.com>
2023-11-17 19:36:51 +0100

    New version with ovelay player

---
+++ sintspel.js
@@ -0,0 +1,48 @@
+var games = {
+    ozosnel: 'pietengames-2019/ozosnel.html',
+    waarozosnel: 'pieten-minigames-2021/ozosnel.html',
+    precisiestrooien: 'pieten-minigames-2020/precisiestrooien.html',
+    babypiet: 'pietengames-2021/babypiet.html',
+    schoenenzoeken: 'pieten-minigames-2020/schoenenzoeken.html',
+    paardvoeren: 'pieten-minigames-2018/paardvoeren.html',
+    chocoladegereedschap: 'pieten-minigames-2021/chocoladegereedschap.html',
+    taartversieren: 'pietengames-2021/taartversieren.html',
+    trefbal: 'pietengames-2016/trefbal.html',
+    burgemeestersketting: 'pieten-minigames-2020/burgemeestersketting.html',
+    pakjeskamer: 'pietengames-2020/pakjeskamer.html',
+    pong: 'pietengames-2016/pong.html',
+    bootspel: 'pietengames-2020/bootspel.html',
+    dakspel: 'pietengames-2016/dakspel.html',
+    schudspel: 'pietengames-2016/schudspel.html',
+    tikspel: 'pietengames-2017/tikspel.html',
+    pepernotenfeest: 'pieten-minigames-2018/pepernotenfeest.html',
+    pietenpetten: 'pieten-minigames-2018/pietenpetten.html',
+    vangspel: 'pietengames-2017/vangspel.html',
+    mallenknallen: 'pietengames-2018/mallenknallen.html',
+    plaatjesbende: 'pietengames-2018/plaatjesbende.html',
+    sjoelen: 'pietengames-2019/sjoelen.html',
+    treinspel: 'pietengames-2019/treinspel.html',
+    verlanglijstje: 'pieten-minigames-2019/verlanglijstje.html',
+    vlekkenpoetsen: 'pieten-minigames-2019/vlekkenpoetsen.html',
+    pietenjump: 'pietengames-2018/pietenjump.html'
+}
+
+let player = document.createElement("iframe");
+selector = document.getElementById("selector");
+overlay = document.getElementById("overlay")
+
+function StartGame(){
+    player.src = "https://cdn-sinterklaasjournaal.ntr.nl/assets/games/2023/" + games[selector.value];
+    player.id = "player";
+    on();
+}
+
+function on() {
+    overlay.appendChild(player)
+    overlay.style.display = "block";
+}
+
+function off() {
+    overlay.style.display = "none";
+    document.getElementById("player").remove();
+} 
